24630c19c5d33f63f999347e89593a730cb0ba97
Shoaib-Ahmad-102 <m.shoaibahmad102@gmail.com>
2024-11-28 18:56:20 +0500

    Strings in JavaScript

---
+++ script.js
@@ -0,0 +1,84 @@
+// String 
+
+// let str = 'ZuhaNaeem';
+
+// string length 
+// str.length 
+
+
+// string index/indices access 
+// console.log(str[0]);
+
+// Template Literals 
+// let obj = {
+//     item: "Pen",
+//     price: 10,
+// };
+// console.log("The price of", obj.item, "is", obj.price, "rupees");
+// // By written Template Literals 
+// console.log(`The price of ${obj.item} is ${obj.price} rupees`);
+
+// Escape Characters 
+// str = "Zuha\nNaeem";
+// console.log(str, str.length);
+// str = "Shoaib\tAhmad";
+// console.log(str, str.length);
+
+
+
+// ----------String Methods in JS---------- 
+// Upper Case 
+// let str = "Shoaibahmad";
+// console.log(str.toUpperCase());
+
+
+// Lower case 
+// let str = "SHOAIBAHMAD";
+// str.toLowerCase();
+// str=str.toLowerCase();
+// let strNew = str.toLowerCase()
+// console.log(strNew);
+
+
+
+// Trim Method 
+// to remove all the empty spaces from start to end in our string 
+// let str = "       Shoaib   Ahmad      ";
+// str = str.trim()
+// 
+// The spaces between the words will not be removed
+
+
+
+// slice Mehod 
+// str.slice(start,end) 
+
+// let str = "ZuhaShoaib";
+// console.log(str.slice(0, 4))
+
+
+
+
+// concatination Method 
+// str1.concat(str2) 
+
+// let str1 = "Zuha";
+// let str2 = "Shoaib";
+
+// console.log(str1.concat(str2));
+
+
+// Find and replace Method 
+// str1.replace("search the value", "New Value")
+
+// let str1 = "Search";
+
+// let str2 = str1.replace("S", "M");
+// console.log(str2)
+
+
+// Practice Question
+// Q. Prompt the user to enter their full name. Generat a username for them based on the input Start username with @, followed by their full name and ending with fullname length.
+// let userName = prompt("Enter your user name");
+// console.log("@"+ userName+ userName.length);
+
