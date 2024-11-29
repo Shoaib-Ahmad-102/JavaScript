e0e99fe372e959d493dc1b585d246d467636ae0e
Shoaib-Ahmad-102 <m.shoaibahmad102@gmail.com>
2024-11-29 19:39:36 +0500

    Functions in JavaScript

---
+++ script.js
@@ -0,0 +1,72 @@
+// function firstFunction(){
+//     console.log("My name is Muhammad Shoaib Ahmad")
+// }
+
+// firstFunction();
+
+// We can call our function multiple times
+
+// firstFunction();
+// firstFunction();
+// firstFunction();
+// firstFunction();
+
+
+// Now pass some parameters and arguments in our function 
+
+// function Function(msg){
+//     console.log(msg);
+// }
+
+// Function('My name is Shoaib Ahmad Bhutta');
+
+
+// We can pass multiple arguments and parametes in our function 
+
+// function sum(x, y){
+//     console.log(x+ y);
+// }
+
+// sum(3, 4);
+
+
+// we can also return our value in the function by puting return in the funstion and then call it 
+
+// function sum(x, y){
+//     s = x + y;
+//     return s;
+// }
+
+// let value = sum(3 + 8);
+// console.log(value);
+
+// Arrow Function 
+
+// arrow funtion is the simplest and easy way to write the function 
+
+// But fist we hav to store arrow funciton in any aspecific variable 
+
+// let arrowSum = (a,b) =>{
+//     console.log(a+b);
+// }
+
+// now if we only write arrowSum. it will give the whole block of code not only the value of a +b.       Let's try;
+// arrowSum  //it will only give us the whole value
+
+// But we have to write arrowSum and the value which will do sum for us.
+// arrowSum(3+4);
+
+// Practice Question about function 
+
+// Create a function using the "function" keywork that takes a string as an argument & returns the number of vowels in the string.
+
+// function countVowels(str){
+//     let count=0;
+//     for (let char of str ){
+//     if(char ==="a" || char ==="e" || char ==="i" || char === "o" || char === "u" ){
+//         count++;
+//     }
+//     }
+//     console.log(count); 
+// }
+
