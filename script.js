c14dd0e21cb54ad848a1d641bc9fdf386bfe1212
Shoaib-Ahmad-102 <m.shoaibahmad102@gmail.com>
2024-11-29 18:48:58 +0500

    Arrays in JavaScript

---
+++ script.js
@@ -0,0 +1,93 @@
+// Arrays basic sytax 
+
+// marks = [57, 26, 93, 71, 69]
+// console.log(marks);
+
+// heroes = ["batman", "hulk", "spidarman", "thor", "ironman"]
+// console.log(heroes);
+
+
+
+// Looping over Array 
+
+// let names = ["Umar", "Usman", "Ali", "Fahad", "Abid", "Zulqar"]
+// for loop 
+
+// for (let i=0; i<names.length; i++){
+//     console.log(names[i]);
+// }
+
+
+// for of loop 
+
+// for (let name of names){
+//     console.log(name);
+// }
+
+// Methods in JavaScript 
+
+// Push Method 
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// city.push("Karaci", "Islamabad", "Peshawar")
+// console.log(city);
+
+
+
+// Pop Method 
+
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// console.log(city);
+// let deletedCity = city.pop();
+// console.log("The Deleted City is: ", deletedCity);
+
+
+
+// To string method 
+
+// converts array into string 
+
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// console.log(city);
+// console.log(city.toString());
+
+// let marks = [57, 26, 93, 71, 69]
+// console.log(marks);
+// console.log(marks.toString());
+
+
+
+// Concat method 
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// let otherCity = ["Gujrat", "Gujranwala", "Sialkot", "Okara"]
+// console.log(city.concat(otherCity));
+
+
+// Unshift Method 
+
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// city.unshift("Sialkot");
+
+
+// Shift Method 
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// console.log(city);
+// console.log("The deleted City from start is: ",city.shift());
+
+
+// Splice Method 
+// let city = ["Hydarabad", "Multan", "Lahore", "Faisalabad", "Sakhar"]
+// console.log(city);
+// city.splice(1, 1, 'okara');
+
+// Add Element
+// city.splice(4, 0, "Qoeta" );
+
+
+// Delete Element 
+// city.splice(0, 1);
+
+// replace Element 
+// city.splice(1, 1, "Peshawar");
+
+
+
